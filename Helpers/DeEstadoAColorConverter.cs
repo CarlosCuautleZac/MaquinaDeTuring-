11d50965eb9d6e39e31f83f22a29bab7e100f062
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-06-03 01:35:18 -0500

    cdf

---
+++ Helpers/DeEstadoAColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaquinaDeTuring.Helpers
+{
+    internal class DeEstadoAColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string Color = "Transparent";
+            if (value.ToString() == "Error")
+            {
+                Color = "#ae0000";
+                return Color; 
+            }
+            else if (value.ToString() == "Codificando")
+            {
+                Color = "#ff6d0a";
+                return Color;
+            }
+            else if (value.ToString()== "Listo")
+            {
+                Color = "#2db83d";
+                return Color;
+            }
+            return Color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Helpers/DeEstadoATextoConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaquinaDeTuring.Helpers
+{
+    internal class DeEstadoATextoConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string Texto = "Transparent";
+            if (value.ToString() == "Error")
+            {
+                Texto = "Error: Recuerda que solo puedes introducir letras.";
+                return Texto;
+            }
+            else if (value.ToString() == "Codificando")
+            {
+                Texto = "En proceso...";
+                return Texto;
+            }
+            else if (value.ToString() == "Listo")
+            {
+                Texto = "Listo";
+                return Texto;
+            }
+            return Texto;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
